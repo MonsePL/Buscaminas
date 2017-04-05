da699d9b6b901e48df7e3fbd500b1e778e45e965
Monserrat Pineda <monserrat.pineda.luna@gmail.com>
2017-04-05 09:01:30 -0500

    estructura html, botones e imagenes

---
+++ buscaminas.js
@@ -0,0 +1,17 @@
+alert("miau")
+
+var imagencitasbom= []
+
+
+
+
+function bomba(){
+  for (var i = 0; i <16; i++) {
+      imagencitasbom(i) ="bomba.jpg";
+  }
+
+
+  function imagenbomba(imagen){
+
+  imagen.src= imagencitasbom[0];
+  }
